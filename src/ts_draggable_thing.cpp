9d5abc263110d01076b5eb2a48b8b63870d31d6d
DiFFtY <diffty@gmail.com>
2017-10-09 02:05:20 +0200

    events overridables, gestion du multiuser et implémentation du rayon d'action et personnalisation du temps de cooldown

--- src/ts_draggable_thing.cpp
+++ src/ts_draggable_thing.cpp
@@ -1,31 +1,77 @@
 #include "ts_draggable_thing.hpp"
 #include "ts_things_manager.hpp"
+#include "ts_main_character.hpp"
 
 
-DraggableThing::DraggableThing(Image* pImg, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, bool bUsableOnce, bool bSingleUser, bool bIsDraggable) :
+DraggableThing::DraggableThing(Image* pImg, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, int iCooldownTime, int iActionRadius, bool bUsableOnce, bool bSingleUser, bool bIsDraggable, bool bInStore) :
 	DraggableSprite(pImg, vPos, pInputManager, bIsDraggable) {
 
-	init(pThingsManager, iAppealPower, iOccupationTime, bUsableOnce, bSingleUser);
+	init(pThingsManager, iAppealPower, iOccupationTime, iCooldownTime, iActionRadius, bUsableOnce, bSingleUser, bInStore);
 }
 
-DraggableThing::DraggableThing(SpriteSheet* pSprSht, uint uFrameNb, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, bool bUsableOnce, bool bSingleUser, bool bIsDraggable) :
+DraggableThing::DraggableThing(SpriteSheet* pSprSht, uint uFrameNb, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, int iCooldownTime, int iActionRadius, bool bUsableOnce, bool bSingleUser, bool bIsDraggable, bool bInStore) :
 	DraggableSprite(pSprSht, uFrameNb, vPos, pInputManager, bIsDraggable) {
 
-	init(pThingsManager, iAppealPower, iOccupationTime, bUsableOnce, bSingleUser);
+	init(pThingsManager, iAppealPower, iOccupationTime, iCooldownTime, iActionRadius, bUsableOnce, bSingleUser, bInStore);
 }
 
 DraggableThing::~DraggableThing() {
-	
+
 }
 
 
-void DraggableThing::init(ThingsManager* pThingsManager, int iAppealPower, int iOccupationTime, bool bUsableOnce, bool bSingleUser) {
+void DraggableThing::init(ThingsManager* pThingsManager, int iAppealPower, int iOccupationTime, int iCooldownTime, int iActionRadius, bool bUsableOnce, bool bSingleUser, bool bInStore) {
+	m_classInfo.setClassTypeName("DraggableThing");
+
 	m_pThingsManager = pThingsManager;
 
 	m_iAppealPower = iAppealPower;
 	m_iOccupationTime = iOccupationTime;
+	m_iCooldownTime = iCooldownTime;
+	m_iActionRadius = iActionRadius;
 	m_bUsableOnce = bUsableOnce;
 	m_bSingleUser = bSingleUser;
+
+	m_bInStore = bInStore;
+
+	int m_type = 0;
+
+	initList(&m_llUsers);
+}
+
+void DraggableThing::registerNewUser(MainCharacter* pCharUser) {
+	addDataToList(&m_llUsers, pCharUser);
+}
+
+void DraggableThing::unregisterNewUser(MainCharacter* pCharUser) {
+	LLNode* pCurrNode = m_llUsers.pHead;
+
+	while (pCurrNode != NULL) {
+		MainCharacter* pCurrCharUser = (MainCharacter*) pCurrNode->pData;
+
+		if (pCharUser == pCurrCharUser) {
+			removeNodeFromList(&m_llUsers, pCurrNode);
+			delete pCurrNode;
+		}
+
+		pCurrNode = pCurrNode->pNext;
+	}
+}
+
+bool DraggableThing::isUsed() {
+	return m_llUsers.size > 0;
+}
+
+void DraggableThing::setIsInStore(bool bInStore) {
+	m_bInStore = bInStore;
+}
+
+bool DraggableThing::isInStore() {
+	return m_bInStore;
+}
+
+bool DraggableThing::isSingleUser() {
+	return m_bSingleUser;
 }
 
 void DraggableThing::onDragStart(vect2d_t vStartDragPt) {
@@ -33,10 +79,39 @@ void DraggableThing::onDragStart(vect2d_t vStartDragPt) {
 }
 
 void DraggableThing::onDragEnd() {
-	m_pThingsManager->addThing(this);
-	m_pThingsManager->renewThingInStore(this);
+	m_pThingsManager->onThingMoved();
+
+	if (m_bInStore) {
+		m_pThingsManager->addThing(this);
+		m_pThingsManager->renewThingInStore(this);
+		m_bInStore = false;
+	}
 }
 
 void DraggableThing::onDragging() {
 	
+}
+
+void DraggableThing::onBeginUsing() {
+	
+}
+
+void DraggableThing::onEndUsing() {
+	
+}
+
+void DraggableThing::onUsing() {
+	
+}
+
+int DraggableThing::getOccupationTime() {
+	return m_iOccupationTime;
+}
+
+int DraggableThing::getActionRadius() {
+	return m_iActionRadius;
+}
+
+int DraggableThing::getCooldownTime() {
+	return m_iCooldownTime;
 }
--- src/ts_draggable_thing.hpp
+++ src/ts_draggable_thing.hpp
@@ -5,31 +5,58 @@
 #include "image.hpp"
 #include "input.hpp"
 #include "draggable_sprite.hpp"
+#include "tiny_progress_bar.hpp"
+#include "linked_list.hpp"
+#include "iobject.hpp"
+
 
 
 class ThingsManager;
+class MainCharacter;
 
 class DraggableThing : public DraggableSprite
 {
-private:
+protected:
 	ThingsManager* m_pThingsManager;
 
 	int m_iAppealPower;
 	int m_iOccupationTime;
+	int m_iCooldownTime;
+	int m_iActionRadius;
 	bool m_bUsableOnce;
 	bool m_bSingleUser;
 
+	bool m_bInStore;
+
+	LinkedList m_llUsers;
+
 public:
-	DraggableThing(Image* pImg, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, bool bUsableOnce, bool bSingleUser, bool bIsDraggable = true);
-	DraggableThing(SpriteSheet* pSprSht, uint uFrameNb, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, bool bUsableOnce, bool bSingleUser, bool bIsDraggable = true);
+	DraggableThing(Image* pImg, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, int iCooldownTime, int iActionRadius, bool bUsableOnce, bool bSingleUser, bool bIsDraggable = true, bool bInStore = false);
+	DraggableThing(SpriteSheet* pSprSht, uint uFrameNb, vect2df_t vPos, ThingsManager* pThingsManager, Input* pInputManager, int iAppealPower, int iOccupationTime, int iCooldownTime, int iActionRadius, bool bUsableOnce, bool bSingleUser, bool bIsDraggable = true, bool bInStore = false);
 	~DraggableThing();
 
-	void init(ThingsManager* pThingsManager, int iAppealPower, int iOccupationTime, bool bUsableOnce, bool bSingleUser);
+	void init(ThingsManager* pThingsManager, int iAppealPower, int iOccupationTime, int iCooldownTime, int iActionRadius, bool bUsableOnce, bool bSingleUser, bool bInStore);
+
+	void registerNewUser(MainCharacter* pCharUser);
+	void unregisterNewUser(MainCharacter* pCharUser);
+	bool isUsed();
+
+	void setIsInStore(bool bInStore);
+	bool isInStore();
+	bool isSingleUser();
 
 	// Overrides du parent
-	void onDragStart(vect2d_t vStartDragPt);
-	void onDragEnd();
-	void onDragging();
+	virtual void onDragStart(vect2d_t vStartDragPt);
+	virtual void onDragEnd();
+	virtual void onDragging();
+
+	virtual void onBeginUsing();
+	virtual void onEndUsing();
+	virtual void onUsing();
+
+	int getOccupationTime();
+	int getActionRadius();
+	int getCooldownTime();
 };
 
 
