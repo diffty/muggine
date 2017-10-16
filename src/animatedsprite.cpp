5efc16691c304c9b32e3571a9dc33bd39ad65a11
DiFFtY <diffty@gmail.com>
2017-10-16 16:59:13 +0200

    On passe par le setter de numéro de frame plutôt que de changer directement l'attribut.

--- src/animatedsprite.cpp
+++ src/animatedsprite.cpp
@@ -17,10 +17,10 @@ AnimatedSprite::AnimatedSprite(SpriteSheet* pSprSht, vect2df_t vPos, float fPlay
     
     if (m_llAnimStates.size > 0) {
         m_pCurrAnimState = (AnimationState*) m_llAnimStates.pHead->pData;
-        m_uFrameNb = m_pCurrAnimState->uFrameStart;
+		setFrame(m_pCurrAnimState->uFrameStart);
     }
     else {
-        m_uFrameNb = 0;
+		setFrame(0);
     }
 }
 
@@ -45,7 +45,7 @@ void AnimatedSprite::addState(AnimationState animState) {
     
     if (m_llAnimStates.size == 1) {
         m_pCurrAnimState = (AnimationState*) m_llAnimStates.pHead->pData;
-        m_uFrameNb = m_pCurrAnimState->uFrameStart;
+		setFrame(m_pCurrAnimState->uFrameStart);
     }
 }
 
@@ -55,7 +55,7 @@ void AnimatedSprite::changeState(uint uStateId) {
     AnimationState* pAnimState = (AnimationState*) pAnimStateNode->pData;
     if (pAnimState != m_pCurrAnimState) {
         m_pCurrAnimState = pAnimState;
-        m_uFrameNb = m_pCurrAnimState->uFrameStart;
+		setFrame(m_pCurrAnimState->uFrameStart);
     }
 }
 
@@ -67,16 +67,18 @@ void AnimatedSprite::update() {
     if (m_pCurrAnimState != NULL) {
         m_fCurrFrameTime += System::get()->getDeltaTime();
         
-        if (m_fCurrFrameTime > (1. / m_pCurrAnimState->uFPS)) {
-            m_uFrameNb++;
+		int iCurrFrame = getFrame();
+		
+		if (m_fCurrFrameTime > (1. / m_pCurrAnimState->uFPS)) {
+			iCurrFrame++;
             
-            if (m_uFrameNb > m_pCurrAnimState->uFrameEnd) {
-                m_uFrameNb = m_pCurrAnimState->uFrameStart;
+            if (iCurrFrame > m_pCurrAnimState->uFrameEnd) {
+				iCurrFrame = m_pCurrAnimState->uFrameStart;
             }
+
+			setFrame(iCurrFrame);
             
             m_fCurrFrameTime = 0.;
         }
     }
-    
-    //printf("%i\n", m_uFrameNb);
 }
